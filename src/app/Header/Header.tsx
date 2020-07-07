1df4612f7196a9f2998b0d0f252de0504a014a49
Ivan Dovzhikov <einegeist@gmail.com>
2020-07-07 23:45:21 +0300

    Add information about amount of games to header

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,8 +1,29 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { sum } from 'lodash';
 import './Header.scss';
+import { AppState } from 'store/types';
+
+interface AmountOfGames {
+  totalGames: number;
+  displayedGames: number;
+}
 
 const Header: FC = () => {
-  return <header className="app-header"></header>;
+  const { totalGames, displayedGames } = useSelector<AppState, AmountOfGames>(
+    ({ games: { allGames, gamesToDisplay } }) => ({
+      totalGames: allGames.length,
+      displayedGames: gamesToDisplay
+        ? sum(gamesToDisplay.map(chunk => chunk.length))
+        : allGames.length,
+    })
+  );
+
+  return (
+    <header className="app-header">
+      {displayedGames}/{totalGames}
+    </header>
+  );
 };
 
 export default Header;
