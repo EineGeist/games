d2d48147cbe5b366a6af66b6875007b7e0a9ccbc
Ivan Dovzhikov <einegeist@gmail.com>
2020-07-07 11:02:46 +0300

    Add root styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
+import 'index.scss';
 import App from 'app/App';
 
 ReactDOM.render(
