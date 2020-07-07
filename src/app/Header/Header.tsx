fe3d5e0ad21ff6572ba7dd8f7976cb80d1a7cdda
Ivan Dovzhikov <einegeist@gmail.com>
2020-07-07 10:11:09 +0300

    Add header

---
+++ src/app/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import './Header.scss';
+
+const Header: FC = () => {
+  return <header className="app-header"></header>;
+};
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import 'normalize.css';
+import App from 'app/App';
 
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}></BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById('app')
 );
